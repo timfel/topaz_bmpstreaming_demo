bafd59fd0a3027092a447fe227889c610422fb06
Tim Felgentreff <timfelgentreff@gmail.com>
2013-01-23 21:27:02 +0100

    rename

--- play.sh
+++ play.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mplayer -demuxer rawvideo -rawvideo w=640:h=480:format=rgb24:fps=15 out.yuv -vo x11
+mplayer -demuxer rawvideo -rawvideo w=640:h=480:format=rgb24:fps=15 out.rgb -vo x11
