12042185deabbeddffcd76430e4a600267b12895
Tim Felgentreff <timfelgentreff@gmail.com>
2013-01-25 12:41:25 +0100

    make reading load work on topaz

--- lib/quality_control.rb
+++ lib/quality_control.rb
@@ -6,21 +6,19 @@ module QualityControl
   def quality_control(quality)
 
     LoadAvg.rewind
-    cpuload = LoadAvg.read(3).to_f
+    cpuload = LoadAvg.read(4).gsub(".", "").to_i
 
     start = Time.now.to_f
     yield
     duration = Time.now.to_f - start
 
     LoadAvg.rewind
-    cpuload_after = LoadAvg.read(3).to_f
+    cpuload_after = LoadAvg.read(4).gsub(".", "").to_i
 
     # Very important: stay fast
     if duration > FrameTime
       quality = quality * (FrameTime / duration)
     end
-
-
 
     return quality
   end
