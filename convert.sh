9de31b7c7eb52bfb86f42eb30731b5026dd76b0f
Tim Felgentreff <timfelgentreff@gmail.com>
2013-01-26 14:05:59 +0100

    drop every other frame

--- convert.sh
+++ convert.sh
@@ -7,5 +7,10 @@ pushd video
 rm *.png
 rm *.bmp
 mplayer -frames 1000 -ao null -ac null -vo png "$path"
-for i in *.png; do convert -flop $i ${i%.*}.bmp; done
+for i in *.png; do
+    if [ 0 -eq "$(expr ${i%.*} % 2)" ]; then
+	convert -flop $i ${i%.*}.bmp
+    fi
+done
+rm *.png
 popd
